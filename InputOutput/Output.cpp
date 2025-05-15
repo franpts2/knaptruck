de6e56f56fc651c114eed9609459eaf9a88b4429
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2025-05-15 10:55:07 +0100

    Added the option handler in the menu and an output file to handle the output of the brute force approach

---
+++ InputOutput/Output.cpp
@@ -0,0 +1,5 @@
+//
+// Created by francisca on 5/15/25.
+//
+
+#include "Output.h"
---
+++ InputOutput/Output.h
@@ -0,0 +1,8 @@
+//
+// Created by francisca on 5/15/25.
+//
+
+#ifndef OUTPUT_H
+#define OUTPUT_H
+
+#endif //OUTPUT_H
